565371cc2b6505a0cedd24181fc95d72cd58f306
ckie <git-525ff67@ckie.dev>
2023-02-12 16:30:22 +0200

    nixos/cnping: add package option

--- nixos/modules/programs/cnping.nix
+++ nixos/modules/programs/cnping.nix
@@ -7,6 +7,12 @@ in
   options = {
     programs.cnping = {
       enable = lib.mkEnableOption "a setcap wrapper for cnping";
+      package = lib.mkOption {
+        default = pkgs.cnping;
+        type = lib.types.package;
+        defaultText = lib.literalExpression "pkgs.cnping";
+        description = "cnping derivation to use";
+      };
     };
   };
 
@@ -14,7 +20,7 @@ in
     security.wrappers.cnping = {
       owner = "root";
       group = "root";
-      source = "${pkgs.cnping}/bin/cnping";
+      source = "${cfg.package}/bin/cnping";
       capabilities = "cap_net_raw+ep";
     };
   };
