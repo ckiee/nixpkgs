88d88fe130bf0853220af2095800bb639aae4c7f
ckie <git-525ff67@ckie.dev>
2024-03-15 17:31:35 +0200

    wivrn: bluhbleh

--- pkgs/by-name/wi/wivrn/package.nix
+++ pkgs/by-name/wi/wivrn/package.nix
@@ -2,6 +2,7 @@
 , stdenv
 , fetchFromGitHub
 , fetchFromGitLab
+, fetchpatch2
 , avahi
 , boost
 , cmake
@@ -35,16 +36,26 @@
 }:
 let
   vendorMonado = monado.overrideAttrs rec {
-    # Version stated in CMakeList for WiVRn 0.11
-    version = "57e937383967c7e7b38b5de71297c8f537a2489d";
+    # Version stated in CMakeList for WiVRn 0.12
+    version = "ffb71af26f8349952f5f820c268ee4774613e200";
 
     src = fetchFromGitLab {
       domain = "gitlab.freedesktop.org";
       owner = "monado";
       repo = "monado";
       rev = version;
-      hash = "sha256-O/Td2WccTr4Fa8U64/lVfnidSIH5t3gWuFXCsEVf7bk=";
+      hash = "sha256-+RTHS9ShicuzhiAVAXf38V6k4SVr+Bc2xUjpRWZoB0c=";
     };
+
+    # FIXME: Never used? Since we only eval .src
+    patches = [
+      # WiVRn-specific patch
+      (fetchpatch2 {
+        name = "0001-c-multi-disable-dropping-of-old-frames.patch";
+        url = "https://raw.githubusercontent.com/Meumeu/WiVRn/46d6d2181b62ecb976616a3b2396b9bd0ce345b4/patches/monado/0001-c-multi-disable-dropping-of-old-frames.patch";
+        hash = "sha256-s/000000000/woCEOEZECdcZoJDoWc1eM63sd60cxeY=";
+      })
+    ];
 
     postInstall = ''
       mv src/xrt/compositor/libcomp_main.a $out/lib/libcomp_main.a
@@ -53,18 +64,22 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "wivrn";
-  version = "0.11";
+  version = "0.12";
 
   src = fetchFromGitHub {
     owner = "meumeu";
     repo = "wivrn";
     rev = "v${version}";
-    hash = "sha256-E48JYhkBVZfb7S/FW0F8RbMtx4GIJwfXfs4KAF3gn8A=";
+    hash = "sha256-O6Eq7EQ427hOcN16Z33I74CevnHlX/a4ZAcljgc+vk8=";
   };
 
-  patchPhase = ''
+  patches = [
+    ./0001-server-wivrn_session-Add-fps-field-to-header.patch
+  ];
+
+  prePatch = ''
     substituteInPlace ./server/CMakeLists.txt \
-      --replace "../../../" "../../../../.."
+      --replace "../../../" "../../../../../.."
   '';
 
   nativeBuildInputs = [
